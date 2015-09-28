1d02ce58b2b32b800b70fd829b5e1244da52db18
Yi Liu <yi9@clemson.edu>
2015-09-27 20:20:18 -0400

    #11. namei.c pvfs2_setattr

--- fs/orangefs/symlink.c
+++ fs/orangefs/symlink.c
@@ -24,7 +24,7 @@ static const char *pvfs2_follow_link(struct dentry *dentry, void **cookie)
 struct inode_operations pvfs2_symlink_inode_operations = {
 	.readlink = generic_readlink,
 	.follow_link = pvfs2_follow_link,
-	.setattr = pvfs2_setattr,
+	.setattr = orangefs_setattr,
 	.getattr = pvfs2_getattr,
 	.listxattr = pvfs2_listxattr,
 	.setxattr = generic_setxattr,
